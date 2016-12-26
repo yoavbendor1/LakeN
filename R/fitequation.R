9eff0e185aee8574d4c0f9150b5e170b277b9baf
yoavbendor1 <yoav.bendor1@mail.huji.ac.il>
2016-12-26 12:29:50 +0200

    modelled NH4 consumption and added some graphics also fixed mixup between a and b parameters

--- R/fitequation.R
+++ R/fitequation.R
@@ -10,9 +10,9 @@
 fitequation <-
   function(input){
       m =input
-      eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2,
-                       list(a = format(coef(m)[1], digits = 2, scientific=TRUE),
-                            b = format(coef(m)[2], digits = 2, scientific=TRUE),
+      eq <- substitute(italic(y) == b + a %.% italic(x)*","~~italic(r)^2~"="~r2,
+                       list(b = format(coef(m)[1], digits = 2, scientific=TRUE),
+                            a = format(coef(m)[2], digits = 2, scientific=TRUE),
                             r2 = format(summary(m)$r.squared, digits = 2)))
       return(as.character(as.expression(eq)))
   }
